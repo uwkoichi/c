ceeb145c6c90c1ddba98973e977adffb45e2d470
Koichi Akama <62323834+uwkoichi@users.noreply.github.com>
2022-06-29 23:09:34 +0900

    浮動小数点

---
+++ chapter_4/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%f\n", 10.0 + 3.0);
+    printf("%f\n", 10.0 - 3.0);
+    printf("%f\n", 10.0 * 3.0);
+    printf("%f\n", 10.0 / 3.0);
+    return 0;
+}
